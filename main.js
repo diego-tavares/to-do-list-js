bbb75889662ea42e0cf1f592cc908225e9f13be2
diego-tavares <cdiegostavares@gmail.com>
2022-09-11 16:04:33 -0300

    add js

---
+++ main.js
@@ -0,0 +1,7 @@
+const texto = document.querySelector('input');
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.head button')
+const ul = document.querySelector('ul')
+
+var itensDB = []
+
