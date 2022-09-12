d3dae10e6a5d1535f9c25119ef3c7d567e140675
Carlos Diego Tavares <102318991+diego-tavares@users.noreply.github.com>
2022-09-12 13:52:53 -0300

    update main.js

--- main.js
+++ main.js
@@ -5,3 +5,38 @@ const ul = document.querySelector('ul')
 
 var itensDB = []
 
+texto.addEventListener('keypress', e => {
+    if (e.key == 'Enter' && texto.value != '') {
+        setItemDB()
+    }
+})
+
+btnInsert.onclick = () => {
+    if (texto.value != '') {
+        setItemDB()
+    }
+}
+
+function setItemDB() {
+    if (itensDB.length >= 20) {
+        alert('limite máximo de 20 itens atingido!')
+        return
+    }
+    
+    itensDB.push({ 'item': texto.value, 'status': ''})
+    updateDB()
+}
+
+function updateDB () {
+    localStorage.setItem('todolist', JSON.stringify(itensDB))
+    loadItens()
+}
+
+function loadItens () {
+    ul.innetHTML = "";
+    itensDB = JSON.parse(localStorage.getItem('todolisst')) ?? []
+    itensDB.array.forEach(item, i => {
+        insertItemTela(item.item, item.status, i)
+    });
+}
+
