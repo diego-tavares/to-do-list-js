042214c2b16094a11735832ea3549f398d516661
diego-tavares <cdiegostavares@gmail.com>
2022-09-14 21:56:56 -0300

    update js

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ btnInsert.onclick = () => {
 }
 
 function setItemDB() {
-  if (itensDB.length >= 20) {
-    alert('Limite máximo de 20 itens atingido!')
+  if (itensDB.length >= 30) {
+    alert('Limite máximo de 30 itens atingido!')
     return
   }
 
